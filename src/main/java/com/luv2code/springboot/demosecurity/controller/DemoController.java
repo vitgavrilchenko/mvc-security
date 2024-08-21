e49f579b88cfedf648cf9bd274d4f2dd48fdf54e
vitgavrilchenko <vitgavrilchenko@gmail.com>
2024-08-21 22:22:38 +0300

    add roles and restrict url access based on roles

--- src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -11,4 +11,16 @@ public class DemoController {
     public String showHome() {
         return "home";
     }
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
+
 }
--- src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -38,11 +38,13 @@ public class DemoSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/showMyLoginPage")
                         .loginProcessingUrl("/authenticateTheUser")
-                        .permitAll()
-                )
+                        .permitAll())
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
